dfecf30a09f86fadd2894f74b4a4e3c0f95938b2
LIY <liy@truit.co.kr>
2021-05-12 09:11:27 +0900

    first commit

---
+++ vue-todo-master/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+
+export default {
+  components: {
+    //es5문법
+    // 'TodoHeader': TodoHeader,
+    // 'TodoInput': TodoInput,
+    // 'TodoList': TodoList,
+    // 'TodoFooter': TodoFooter
+    //es6문법 : 왼쪽 오른쪽 같을 경우 생략 가능
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter,
+  },
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
